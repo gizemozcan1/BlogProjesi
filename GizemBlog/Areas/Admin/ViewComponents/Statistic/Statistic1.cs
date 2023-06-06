dffd6d1b14ca57b7f4c1269e314240574b92ec07
Gizem <gizemm.ozcann0@gmail.com>
2023-06-06 19:35:38 +0300

    hava durumu api ile cekildi ve yazarlar ajax ile cekildi.

--- GizemBlog/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
+++ GizemBlog/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -4,6 +4,7 @@ using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
+using System.Xml.Linq;
 
 namespace GizemBlog.Areas.Admin.ViewComponents.Statistic
 {
@@ -16,6 +17,11 @@ namespace GizemBlog.Areas.Admin.ViewComponents.Statistic
             ViewBag.v1 = bm.GetList().Count();
             ViewBag.v2 = c.Contacts.Count();
             ViewBag.v3 = c.Comments.Count();
+
+            string api = "e77b4ce0ecc73b1c210d0d7ed8aad2f4";
+            string connection = "https://api.openweathermap.org/data/2.5/weather?q=istanbul&mode=xml&lang=tr&units=metric&appid="+api;
+            XDocument document= XDocument.Load(connection);
+            ViewBag.v4 = document.Descendants("temperature").ElementAt(0).Attribute("value").Value;
             return View();
         }
     }
