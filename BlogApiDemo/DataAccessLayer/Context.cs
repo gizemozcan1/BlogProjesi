9178ca03b635aca7295367ebc87af18d4b71b5de
Gizem <gizemm.ozcann0@gmail.com>
2023-06-07 19:57:49 +0300

    apilerle projenin haberlestirilmesi

---
+++ BlogApiDemo/DataAccessLayer/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlogApiDemo.DataAccessLayer
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=201-11; database=GizemBlogApiDb; integrated security=true;");
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
